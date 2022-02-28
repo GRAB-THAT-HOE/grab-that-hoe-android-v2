2fb77d9bbfb996a55c471a494387be2d6ffb855b
snack655 <cmjcmj1230@gmail.com>
2022-02-28 15:10:47 +0900

    Delete : Project
    2022-02-28

--- GrabThatHoe2/data/src/main/java/com/moreversal/android/data/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.moreversal.android.data
-
-class MyClass {
-}
--- GrabThatHoe2/presentation/src/main/java/com/moreversal/android/grabthathoe/viewmodel/MainViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.moreversal.android.grabthathoe.viewmodel
-
-import com.moreversal.android.grabthathoe.base.BaseViewModel
-
-class MainViewModel: BaseViewModel() {
-}

93aaee102742ddd6cb0c1d182558226dc6756342
최민재 <cmjcmj1230@gmail.com>
2022-12-05 17:46:17 +0900

    ✅ :: KtLint Gradle

--- presentation/src/main/java/kr/co/moreversal/grabthathoe/scenes/MainActivity.kt
+++ presentation/src/main/java/kr/co/moreversal/grabthathoe/scenes/MainActivity.kt
@@ -1,7 +1,7 @@
 package kr.co.moreversal.grabthathoe.scenes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kr.co.moreversal.grabthathoe.R
 
 class MainActivity : AppCompatActivity() {

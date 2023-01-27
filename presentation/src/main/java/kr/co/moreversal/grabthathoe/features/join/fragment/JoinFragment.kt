893879c10b1bddf4bb58b4a6ba98530e54515ed7
bhindor <bhindor@dgsw.hs.kr>
2023-01-27 15:06:27 +0900

    ⚡️ :: Connect View
    뷰 연결(데이터 처리 하나도 없이)

--- presentation/src/main/java/kr/co/moreversal/grabthathoe/features/join/fragment/JoinFragment.kt
+++ presentation/src/main/java/kr/co/moreversal/grabthathoe/features/join/fragment/JoinFragment.kt
@@ -1,12 +1,18 @@
 package kr.co.moreversal.grabthathoe.features.join.fragment
 
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.co.moreversal.grabthathoe.R
 import kr.co.moreversal.grabthathoe.base.BaseFragment
 import kr.co.moreversal.grabthathoe.databinding.FragmentJoinBinding
+import kr.co.moreversal.grabthathoe.extensions.repeatOnStarted
 import kr.co.moreversal.grabthathoe.features.join.viewmodel.JoinViewModel
+import kr.co.moreversal.grabthathoe.features.plaza.viewmodel.PlazaViewModel
 
 @AndroidEntryPoint
 class JoinFragment : BaseFragment<FragmentJoinBinding, JoinViewModel>(R.layout.fragment_join) {
@@ -14,8 +20,66 @@ class JoinFragment : BaseFragment<FragmentJoinBinding, JoinViewModel>(R.layout.f
     override val viewModel: JoinViewModel by viewModels()
 
     override fun start() {
-        viewModel.name.observe(this) {
-            Log.d("TestTag", it)
+        viewModel.cnt = 0
+        repeatOnStarted {
+            viewModel.eventFlow.collect { event -> handleEvents(event) }
+        }
+        observeLiveData()
+    }
+
+    private fun handleEvents(event: JoinViewModel.Event) {
+        when (event) {
+            is JoinViewModel.Event.ShowNext -> showPage(event.nextCount)
+            is JoinViewModel.Event.ShowPrev -> showPage(event.prevCount)
+            }
+
+        }
+
+    private fun observeLiveData() = with(viewModel) {
+        name.observe(this@JoinFragment) {
+            binding.btnNext.isEnabled = it.isNotBlank()
+        }
+        phoneNum.observe(this@JoinFragment) {
+            binding.btnReceive.isEnabled = it.isNotBlank()
+        }
+        checkNum.observe(this@JoinFragment) {
+            binding.btnCheckNum.isEnabled = it.isNotBlank()
+        }
+        birth.observe(this@JoinFragment) {
+
+        }
+    }
+
+    private fun showPage(count: Int) {
+        Log.d("TestFun", count.toString())
+        when (count) {
+            -1 -> {
+                Toast.makeText(requireContext(), "로그인 페이지로 돌아가기", Toast.LENGTH_SHORT).show()
+            }
+            0 -> {
+                binding.groupPhoneNum.isInvisible = true
+                binding.groupName.isInvisible = false
+                binding.groupNextBtn.isInvisible = false
+            }
+            1 -> {
+                binding.groupName.isInvisible = true
+                binding.groupNextBtn.isInvisible = true
+                binding.groupPhoneNum.isInvisible = false
+                binding.groupCheckNum.isInvisible = true
+            }
+            2 -> {
+                binding.groupPhoneNum.isInvisible = false
+                binding.groupCheckNum.isInvisible = false
+                binding.groupProfile.isInvisible = true
+                binding.groupNextBtn.isInvisible = true
+                //TODO 인증 문자 로직
+            }
+            3 -> {
+                binding.groupPhoneNum.isInvisible = true
+                binding.groupCheckNum.isInvisible = true
+                binding.groupProfile.isInvisible = false
+                binding.groupNextBtn.isInvisible = false
+            }
         }
     }
 }
--- presentation/src/main/java/kr/co/moreversal/grabthathoe/features/join/viewmodel/JoinViewModel.kt
+++ presentation/src/main/java/kr/co/moreversal/grabthathoe/features/join/viewmodel/JoinViewModel.kt
@@ -1,15 +1,44 @@
 package kr.co.moreversal.grabthathoe.features.join.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import kr.co.moreversal.grabthathoe.base.BaseViewModel
+import kr.co.moreversal.grabthathoe.features.plaza.viewmodel.PlazaViewModel
+import kr.co.moreversal.grabthathoe.utils.MutableEventFlow
+import kr.co.moreversal.grabthathoe.utils.asEventFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class JoinViewModel @Inject constructor() : BaseViewModel() {
 
+    private val _eventFlow = MutableEventFlow<JoinViewModel.Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
     val name = MutableLiveData<String>()
+    val phoneNum = MutableLiveData<String>()
+    val checkNum = MutableLiveData<String>()
+    val birth = MutableLiveData<String>()
+
+    var cnt = 0
+
+    fun showNext() {
+        event(Event.ShowNext(++cnt))
+    }
+    fun showPrev() {
+        event(Event.ShowPrev(--cnt))
+    }
+
+    private fun event(event: Event) = viewModelScope.launch {
+        _eventFlow.emit(event)
+    }
 
 
+    sealed class Event {
+        data class ShowNext(val nextCount: Int) : Event()
+        data class ShowPrev(val prevCount: Int) : Event()
+    }
 
 }
